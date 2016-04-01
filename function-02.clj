54f2d4d1fb359eff00f6415b754a1f92cd5426c2
Paul Ramnora <pramnora@yahoo.com>
2016-04-01 09:11:15 +0100

    Update function-02.clj
    Added some further explanatory comments to the code.

--- function-02.clj
+++ function-02.clj
@@ -1,2 +1,9 @@
+;map, is a higher order function...meaning it takes another function as argument
+
+;vector, is a sequential and ordered collection
+
+;passing 'map' the 'inc' function followed by a vector...  
 (map inc [1 2 3 4]); (2 3 4 5)
+
+;passing 'map' the 'dec' function followed by a vector...  
 (map dec [2 3 4 5]); (1 2 3 4)
